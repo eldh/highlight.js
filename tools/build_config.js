cff09a933f433eacda5550bcf085843765819e63
eldh <andreas.eldh@gmail.com>
2020-11-03 11:51:16 +0100

    build esm

--- tools/build_config.js
+++ tools/build_config.js
@@ -1,13 +1,13 @@
-const cjsPlugin = require('rollup-plugin-commonjs');
-const jsonPlugin = require('rollup-plugin-json');
+const cjsPlugin = require('rollup-plugin-commonjs')
+const jsonPlugin = require('rollup-plugin-json')
 
 module.exports = {
-  build_dir: "build",
-  copyrightYears: "2006-2020",
+  build_dir: 'build',
+  copyrightYears: '2006-2020',
   clean_css: {},
   rollup: {
     node: {
-      output: { format: "cjs", strict: false, exports: "auto" },
+      output: { format: 'esm' },
       input: {
         plugins: [
           cjsPlugin(),
@@ -16,49 +16,47 @@ module.exports = {
             transform: (x) => {
               if (/var module/.exec(x)) {
                 // remove shim that only breaks things for rollup
-                return x.replace(/var module\s*=.*$/m, "");
+                return x.replace(/var module\s*=.*$/m, '')
               }
-            }
-          }
+            },
+          },
         ],
       },
     },
     browser_core: {
       input: {
-        plugins: [jsonPlugin()]
+        plugins: [jsonPlugin()],
       },
       output: {
-        name: "hljs",
-        format: "iife",
-        footer: "if (typeof exports === 'object' && typeof module !== 'undefined') { module.exports = hljs; }",
-        interop: false
-      }
+        name: 'hljs',
+        format: 'iife',
+        footer:
+          "if (typeof exports === 'object' && typeof module !== 'undefined') { module.exports = hljs; }",
+        interop: false,
+      },
     },
     browser: {
       input: {
-        plugins: [
-          cjsPlugin(),
-          jsonPlugin()
-        ]
+        plugins: [cjsPlugin(), jsonPlugin()],
       },
       output: {
-        format: "iife",
-        outro: "return module.exports.definer || module.exports;",
-        interop: false
-      }
-    }
+        format: 'iife',
+        outro: 'return module.exports.definer || module.exports;',
+        interop: false,
+      },
+    },
   },
   terser: {
     format: {
       max_line_len: 80,
-      ascii_only: true
+      ascii_only: true,
     },
     compress: {
       passes: 2,
       unsafe: true,
       warnings: true,
       dead_code: true,
-      toplevel: "funcs"
-    }
-  }
-};
+      toplevel: 'funcs',
+    },
+  },
+}
